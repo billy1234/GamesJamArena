ed416c90132f94db5f3a51445f091331581fe168
Jacob Oates <concealedking@gmail.com>
2015-10-16 19:37:25 +1100

    Health added and Test Level

---
+++ QualiaJam/Assets/Health.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour 
+{
+	public int maxHealth;
+	private int health;
+	// Use this for initialization
+	void Start () 
+	{
+		health += maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		checkHealth();
+	}
+	
+	private void checkHealth()
+	{
+		if(health <= 0)
+		{
+			death ();
+		}	
+	}
+	
+	public void takeDamage(int damageAmount)
+	{
+		health -= damageAmount;
+	}
+	
+	protected virtual void death()
+	{
+		print ("Player Died");
+	}
+}
