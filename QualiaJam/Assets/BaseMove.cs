7ab564d8b4e93b878e72a54494bdff48ecb14dfa
Jacob Oates <concealedking@gmail.com>
2015-10-16 19:14:49 +1100

    Base movement and Player Movement with debug controls (W, A, S, D)

---
+++ QualiaJam/Assets/BaseMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+abstract public class BaseMove : MonoBehaviour
+{
+	protected Rigidbody rb;
+	public float speed;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = this.gameObject.GetComponent<Rigidbody>();
+	}
+
+	protected void move(Vector3 direction, float speed)
+	{
+		rb.AddForce(direction * speed);
+	}
+}
---
+++ QualiaJam/Assets/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : BaseMove 
+{
+	void Update()
+	{
+		debugControls();
+	}
+	
+	private void debugControls()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			move(Vector3.forward, speed);
+		}
+		else if (Input.GetKey(KeyCode.S))
+		{
+			move(-Vector3.forward, speed);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			move(-Vector3.right, speed);
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			move(Vector3.right, speed);
+		}
+	}
+	
+}
