cb550aed329519268361d0a85e4a982f5bb743a3
Jacob Oates <concealedking@gmail.com>
2015-10-17 09:36:44 +1100

    Added HealthPickUp and Minion prefab with particle

---
+++ QualiaJam/Assets/Scripts/PickUp/HealthPickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickUp : MonoBehaviour
+{
+	public int healthBonus;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			other.gameObject.GetComponent<Health>().addHealth(healthBonus);
+		}
+	}
+}
