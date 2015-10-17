40f69efd4541cb68c04bffd2dbe929879c49d540
Jacob Oates <concealedking@gmail.com>
2015-10-17 23:35:32 +1100

    Scrolling Texture

---
+++ QualiaJam/Assets/Scripts/ScrollTexture.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTexture : MonoBehaviour 
+{
+	public Material tiledTexture;
+	public float scrollSpeed;
+
+	void Update () 
+	{
+		scrollTexture();
+	}
+	
+	private void scrollTexture()
+	{
+		float offSet = Time.time * scrollSpeed;
+		tiledTexture.mainTextureOffset = new Vector2(offSet,0);
+	}
+	
+	
+}
